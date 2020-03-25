ba9424f377baa45208c23fe71c7c5b40aa47c5af
achuanya <achuan@achuan.io>
2020-03-25 07:09:15 -0400

    上传个人配置

--- api/config.php
+++ api/config.php
@@ -3,7 +3,7 @@
  * 配置文件
  *
  * @author   fooleap <fooleap@gmail.com>
- * @version  2019-04-19 13:23:37
+ * @version  2019-09-18 16:57:31
  * @link     https://github.com/fooleap/disqus-php-api
  *
  */
@@ -46,14 +46,25 @@ define('USE_TEMP', true);
 /*
  * 网络设置
  *
+ * PROXY_MODE     代理模式，可用于墙内服务器，有代理服务器的情况下。优先级高于 IP_MODE，设置为 true，IP_MODE 将忽略
+ * PROXY          代理地址，CURLOPT_PROXY
+ * PROXYTYPE      代理类型，CURLOPT_PROXYTYPE
+ * PROXYUSERPWD   代理账号，CURLOPT_PROXYUSERPWD
+ *
  * IP_MODE        IP 模式，在墙内可尝试设置为 true，将指定 IP，不保证能够访问 Disqus API
  * DISQUS_IP      disqus.com IP 地址，可选：151.101.0.134, 151.101.64.134, 151.101.128.134, 151.101.192.134
  * DISQUS_MEDIAIP uploads.services.disqus.com IP 地址，可选：151.101.24.64, 151.101.40.64, 151.101.52.64
  * DISQUS_LOGINIP import.disqus.com IP 地址，可选：151.101.40.134
  *
  */
+
+define('PROXY_MODE', false);
+define('PROXY', '127.0.0.1:1080');
+define('PROXYTYPE', CURLPROXY_SOCKS5_HOSTNAME);
+define('PROXYUSERPWD', '');
+
 define('IP_MODE', false);
-define('DISQUS_IP', '151.101.0.134');
+define('DISQUS_IP', '151.101.0.134'); 
 define('DISQUS_MEDIAIP', '151.101.24.64');
 define('DISQUS_LOGINIP', '151.101.40.134');
 
@@ -61,12 +72,13 @@ define('DISQUS_LOGINIP', '151.101.40.134');
  * 图片设置
  *
  * GRAVATAR_CDN     Gravatar 头像 CDN
- * GRAVATAR_DEFAULT Gravatar 默认头像，即 d 参数，可参考 https://www.gravatar.com/site/implement/images/
+ * GRAVATAR_DEFAULT Gravatar 默认头像，即 d 参数，可参考 https://www.gravatar.com/site/implement/images/ 
  * EMOJI_PATH       Emoji 表情 PNG 资源路径
  *
  */
-
+ 
 define('GRAVATAR_CDN', '//cn.gravatar.com/avatar/');
+// define('GRAVATAR_DEFAULT', 'retro');
 define('GRAVATAR_DEFAULT', 'mp');
 define('EMOJI_PATH', 'https://github.githubassets.com/images/icons/emoji/unicode/');
 
